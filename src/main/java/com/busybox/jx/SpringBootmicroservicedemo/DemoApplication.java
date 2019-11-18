a74a9978010b590972b32c439533521bca058af6
Ahmed <ahhosni@vistaprint.com>
2019-11-18 11:40:10 +0100

    Initial Commit for SpringBoot microservice demo

---
+++ src/main/java/com/busybox/jx/SpringBootmicroservicedemo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.busybox.jx.SpringBootmicroservicedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
